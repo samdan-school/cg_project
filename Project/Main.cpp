a2f356a0fd1150b742e5d0c04970727a52b8c0e5
Gantugs <gantugs01300130@gmail.com>
2018-12-17 11:36:26 +0800

    m

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -4,6 +4,9 @@
 #include<string.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+
+#include "World.h"
+
 #define PI 3.14159265
 
 using namespace std;
@@ -96,62 +99,7 @@ void displayMe(void)
     gluLookAt(	posx, posy, posz,
 			0, 0, 0,
 			normalx, normaly,  normalz);
-    glColor3f(0.0f, 0.5f, 0.4f);
-
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0,-500);
-    glVertex3f(-500,0,500);
-    glVertex3f(500,0,500);
-    glVertex3f(500,0,-500);
-
-    glEnd();
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0.5,-500);
-    glVertex3f(-500,0.5,-420);
-    glVertex3f(500,0.5,-420);
-    glVertex3f(500,0.5,-500);
-
-    glEnd();
-    glColor3f(0.86f, 0.82f, 0.71f);
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0.5,-420);
-    glVertex3f(-500,0.5,-260);
-    glVertex3f(500,0.5,-260);
-    glVertex3f(500,0.5,-420);
-
-    glEnd();
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0.5,-200);
-    glVertex3f(-500,0.5,-100);
-    glVertex3f(500,0.5,-100);
-    glVertex3f(500,0.5,-200);
-
-    glEnd();
-
-    glColor3f(0.5f, 0.5f, 0.5f);
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0.5,-100);
-    glVertex3f(-500,0.5,100);
-    glVertex3f(500,0.5,100);
-    glVertex3f(500,0.5,-100);
-
-    glEnd();
-     glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_QUADS);
-
-    glVertex3f(-500,0.5,100);
-    glVertex3f(-500,0.5,150);
-    glVertex3f(500,0.5,150);
-    glVertex3f(500,0.5,100);
-
-    glEnd();
+    world_init();
     
 	glPopMatrix();
 	glutSwapBuffers();
--- Project/World.h
+++ Project/World.h
@@ -1,8 +1,66 @@
-#pragma once
-class World
-{
-public:
-	World();
-	~World();
-};
+#include <vector>
+#include<iostream>
+#include <math.h>
+#include<string.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+void world_init() {
+	glColor3f(0.0f, 0.5f, 0.4f);
+
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0,-500);
+    glVertex3f(-500,0,500);
+    glVertex3f(500,0,500);
+    glVertex3f(500,0,-500);
+
+    glEnd();
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0.5,-500);
+    glVertex3f(-500,0.5,-420);
+    glVertex3f(500,0.5,-420);
+    glVertex3f(500,0.5,-500);
+
+    glEnd();
+    glColor3f(0.86f, 0.82f, 0.71f);
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0.5,-420);
+    glVertex3f(-500,0.5,-260);
+    glVertex3f(500,0.5,-260);
+    glVertex3f(500,0.5,-420);
+
+    glEnd();
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0.5,-200);
+    glVertex3f(-500,0.5,-100);
+    glVertex3f(500,0.5,-100);
+    glVertex3f(500,0.5,-200);
+
+    glEnd();
+
+    glColor3f(0.5f, 0.5f, 0.5f);
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0.5,-100);
+    glVertex3f(-500,0.5,100);
+    glVertex3f(500,0.5,100);
+    glVertex3f(500,0.5,-100);
+
+    glEnd();
+     glColor3f(1.0f, 1.0f, 1.0f);
+    glBegin(GL_QUADS);
+
+    glVertex3f(-500,0.5,100);
+    glVertex3f(-500,0.5,150);
+    glVertex3f(500,0.5,150);
+    glVertex3f(500,0.5,100);
+
+    glEnd();
+}
+
 
